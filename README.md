8135a32cf0922765808021273425da36066a5268
Oscar Fogelberg <ofogelberg@gmail.com>
2024-09-22 07:12:10 +0200

    Minor fixes and improvements

--- README.md
+++ README.md
@@ -2,3 +2,28 @@
 
 # hafloorplangen
 Small tool for populating floorplans svg documents and generating rules from items fetched from the HA server.
+
+##  Installation
+
+This is a node script so node and npm is needed. 
+https://nodejs.org/en/learn/getting-started/how-to-install-nodejs
+
+Install the dependencies for this application
+open a terminal in the folder the application is located: and run: npm install
+
+
+## Running
+The application needs to be passed:
+	An svg file of the floor plan
+	A rules file
+	Url to your home assistant server
+	A home assistant long lived token to have permission to access the server
+	https://www.home-assistant.io/docs/authentication/
+	
+
+## Rules
+The rules are a very simple construct 
+
+	
+### Example
+ node genfloorplan.js -s example/planer.svg -r example/rules.yml -u http://www.example.com -t 12345678 
--- genfloorplan.js
+++ genfloorplan.js
@@ -113,7 +113,8 @@ if (!allSet || cmdOptions.help) {
         - The SVG file will be backed up before processing. 
 
         - The rules file is a yaml file that contains the rules for the entities to be added to the SVG.
-          Created rules are stored a ha_rules.yml file in the working directory.
+          Created rules to be included in the HA Floorplan configuration are stored a ha_rules.yml file 
+          in the working directory.
         `,
     },
     {
@@ -148,7 +149,18 @@ if (!allSet || cmdOptions.help) {
   }
 
   const q = url.parse(cmdOptions.url, true);
-  const protocol = q.protocol === 'http' ? http : https;
+  // test if the url is valid
+  if (!q.hostname) {
+    console.error('Invalid URL');
+  }
+
+  // test if the protocol is http or https
+  if (q.protocol !== 'http:' && q.protocol !== 'https:') {
+    console.error('Invalid protocol');
+  }
+
+
+  const protocol = q.protocol === 'http:' ? http : https;
   const requestOptions = {
     path: '/api/states',
     host: q.hostname,
@@ -162,125 +174,124 @@ if (!allSet || cmdOptions.help) {
   let entities = [];
   let data = '';
   const haFloorplanRules = [];
-  try {
-    const req = protocol.request(requestOptions, (resp) => {
-      console.log('Fetching entities from Home Assistant');
-      // A chunk of data has been received.
-      resp.on('data', (chunk) => {
-        data += chunk;
-      });
-
-      // The whole response has been received
-      resp.on('end', () => {
-        entities = JSON.parse(data);
-        const entityIDs = entities.map((e) => e.entity_id);
-
-        console.info(`Received: ${entities.length} entities`);
-
-        // iterate the types that there we want rules for
-
-        for (const rule of rules) {
-          const svgPrimitive = rule.svg_primitive || rule.type;
-          let layerSVGElement = svgDoc.get(
-            `//*[@inkscape:label='${svgPrimitive}']`,
-            namespaces,
-          );
-
-          // validate yaml
-          try {
-            yaml.load(rule.rule_snippet);
-          } catch (err) {
-            console.error(
-              `Error in rule snippet for ${rule.type}: ${err.message}`,
-            );
-          }
-
-          if (!layerSVGElement) {
-            console.info(`Layer ${rule.type} does not exist - creating it`);
-            layerSVGElement = svgDoc.root().node('g');
-            layerSVGElement.attr({
-              'inkscape:groupmode': 'layer',
-              id: `layer_${svgPrimitive}`,
-              'inkscape:label': svgPrimitive,
-            });
-          }
-
-          // filter entities
-          let ruleEntities = entityIDs.filter((e) => e.split('.')[0] === rule.type);
-
-          // filter by attribute
-          if (rule.attribute) {
-            ruleEntities = ruleEntities.filter(
-              (e) => entities.find((ee) => ee.entity_id === e).attributes
-                .device_class === rule.attribute.device_class,
-            );
-          }
-
-          // filter by friendly name
-          if (rule.friendly_name_includes) {
-            ruleEntities = ruleEntities.filter((e) => entities
-              .find((ee) => ee.entity_id === e)
-              .attributes.friendly_name.toLowerCase()
-              .includes(rule.friendly_name_includes));
-          }
-
-          console.info(
-            `Found ${ruleEntities.length} entities of type ${rule.type
-            }, attribute ${rule.attribute ? JSON.stringify(rule.attribute) : '<none>'
-            }, friendly_name_includes: ${rule.friendly_name_includes
-              ? rule.friendly_name_includes
-              : '<none>'
-            }`,
-          );
-
-          // Generate the rule part
-          rule.rules.entities = ruleEntities;
-          haFloorplanRules.push(rule.rules);
-
-          let svgSnippets = svgDoc.find(
-            `//*[@inkscape:label='floorplan.${svgPrimitive}']`,
-            namespaces,
-          );
-
-          if (!svgSnippets || svgSnippets.length === 0) {
-            svgSnippets = svgDoc.find(
-              `//*[@id='floorplan.${svgPrimitive}']`,
-              namespaces,
-            );
-          }
-
-          if (svgSnippets.length > 1) {
-            console.warn('More than one svg snippet found');
-          }
-          if (svgSnippets.length === 0) {
-            console.error(`No svg snippet for ${rule.type} found`);
-          }
-
-          const svgSnippet = svgSnippets[0];
-          // Generate the svg part
-          ruleEntities.forEach((e) => {
-            if (!svgDoc.get(`//*[@id='${e}']`)) {
-              layerSVGElement.addChild(
-                svgSnippet.clone().attr({ id: e, 'inkscape:label': e }),
-              );
-              console.info(`Entity ${e} has been added to SVG`);
-            } else {
-              console.info(`Entity ${e} already exists in SVG`);
-            }
-          });
-        }
-        fs.writeFileSync(svgFileName, svgDoc.toString());
-        fs.writeFileSync(
-          `${__dirname}/ha_rules.yml`,
-          yaml.dump(haFloorplanRules, { lineWidth: 1000 }),
-        );
-      });
+  const req = protocol.request(requestOptions, (resp) => {
+    console.log('Fetching entities from Home Assistant');
+    // A chunk of data has been received.
+    resp.on('data', (chunk) => {
+      data += chunk;
     });
 
-    req.end();
-  } catch (err) {
+    // The whole response has been received
+    resp.on('end', () => {
+      entities = JSON.parse(data);
+      const entityIDs = entities.map((e) => e.entity_id);
+
+      console.info(`Received: ${entities.length} entities`);
+
+      // iterate the types that there we want rules for
+
+      for (const rule of rules) {
+        const svgPrimitive = rule.svg_primitive || rule.type;
+        let layerSVGElement = svgDoc.get(
+          `//*[@inkscape:label='${svgPrimitive}']`,
+          namespaces,
+        );
+
+        // validate yaml
+        try {
+          yaml.load(rule.rule_snippet);
+        } catch (err) {
+          console.error(
+            `Error in rule snippet for ${rule.type}: ${err.message}`,
+          );
+        }
+
+        if (!layerSVGElement) {
+          console.info(`Layer ${rule.type} does not exist - creating it`);
+          layerSVGElement = svgDoc.root().node('g');
+          layerSVGElement.attr({
+            'inkscape:groupmode': 'layer',
+            id: `layer_${svgPrimitive}`,
+            'inkscape:label': svgPrimitive,
+          });
+        }
+
+        // filter entities
+        let ruleEntities = entityIDs.filter((e) => e.split('.')[0] === rule.type);
+
+        // filter by attribute
+        if (rule.attribute) {
+          ruleEntities = ruleEntities.filter(
+            (e) => entities.find((ee) => ee.entity_id === e).attributes
+              .device_class === rule.attribute.device_class,
+          );
+        }
+
+        // filter by friendly name
+        if (rule.friendly_name_includes) {
+          ruleEntities = ruleEntities.filter((e) => entities
+            .find((ee) => ee.entity_id === e)
+            .attributes.friendly_name.toLowerCase()
+            .includes(rule.friendly_name_includes));
+        }
+
+        console.info(
+          `Found ${ruleEntities.length} entities of type ${rule.type
+          }, attribute ${rule.attribute ? JSON.stringify(rule.attribute) : '<none>'
+          }, friendly_name_includes: ${rule.friendly_name_includes
+            ? rule.friendly_name_includes
+            : '<none>'
+          }`,
+        );
+
+        // Generate the rule part
+        rule.rules.entities = ruleEntities;
+        haFloorplanRules.push(rule.rules);
+
+        let svgSnippets = svgDoc.find(
+          `//*[@inkscape:label='floorplan.${svgPrimitive}']`,
+          namespaces,
+        );
+
+        if (!svgSnippets || svgSnippets.length === 0) {
+          svgSnippets = svgDoc.find(
+            `//*[@id='floorplan.${svgPrimitive}']`,
+            namespaces,
+          );
+        }
+
+        if (svgSnippets.length > 1) {
+          console.warn('More than one svg snippet found');
+        }
+        if (svgSnippets.length === 0) {
+          console.error(`No svg snippet for ${rule.type} found`);
+        }
+
+        const svgSnippet = svgSnippets[0];
+        // Generate the svg part
+        ruleEntities.forEach((e) => {
+          if (!svgDoc.get(`//*[@id='${e}']`)) {
+            layerSVGElement.addChild(
+              svgSnippet.clone().attr({ id: e, 'inkscape:label': e }),
+            );
+            console.info(`Entity ${e} has been added to SVG`);
+          } else {
+            console.info(`Entity ${e} already exists in SVG`);
+          }
+        });
+      }
+      fs.writeFileSync(svgFileName, svgDoc.toString());
+      fs.writeFileSync(
+        `${__dirname}/ha_rules.yml`,
+        yaml.dump(haFloorplanRules, { lineWidth: 1000 }),
+      );
+    });
+  });
+
+  req.end();
+
+  req.on('error', (e) => {
     console.error(
-      'Failed to get entities - please ensure that Home Assistant server is available and that the long lived token is correct.',
-    );
-  }
+      'Failed to get entities - please ensure that Home Assistant server is available and that the long lived token is correct.', e);
+  });
 }
